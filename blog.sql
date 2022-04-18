d2745c4f9a085bc7dfc065e99900656fced0e1cf
francoriquelme <francoestebanru@gmail.com>
2022-04-18 08:40:49 -0400

    Finished

---
+++ blog.sql
@@ -0,0 +1,88 @@
+-- Requerimientos
+\c francoriquelme
+DROP DATABASE blog;
+-- 1. Crear base de datos llamada blog.
+CREATE DATABASE blog;
+\c blog
+-- 2. Crear las tablas indicadas de acuerdo al modelo de datos.
+CREATE TABLE usuario(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE
+);
+
+\copy usuario FROM 'usuarios.csv' csv header;
+SELECT * FROM usuario;
+
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY,
+    usuario_id INT NOT NULL REFERENCES usuario(id),
+    titulo VARCHAR(100) NOT NULL UNIQUE,
+    fecha DATE NOT NULL
+);
+
+\copy post FROM 'posts.csv' csv header;
+SELECT * FROM post;
+
+CREATE TABLE comentario(
+    id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL REFERENCES post(id),
+    usuario_id INT NOT NULL REFERENCES usuario(id),
+    texto VARCHAR(30) NOT NULL, 
+    fecha DATE NOT NULL
+);
+
+\copy comentario FROM 'comentarios.csv' csv header;
+SELECT * FROM comentario;
+
+-- 3. Insertar los siguientes registros
+\copy usuario FROM 'usuarios.csv' csv header;
+\copy post FROM 'posts.csv' csv header;
+\copy comentario FROM 'comentarios.csv' csv header;
+-- 4. Seleccionar el correo, id y título de todos los post publicados por el usuario 5.
+SELECT usuario.email, post.id, post.titulo
+FROM usuario
+JOIN post 
+ON usuario.id = post.usuario_id
+WHERE usuario.id = 5;
+-- 5. Listar el correo, id y el detalle de todos los comentarios que no hayan sido realizados
+-- por el usuario con email usuario06@hotmail.com.
+SELECT usuario.email, comentario.id, comentario.texto
+FROM usuario
+JOIN comentario 
+ON comentario.usuario_id = usuario.id
+WHERE usuario.email != 'usuario06@hotmail.com';
+-- 6. Listar los usuarios que no han publicado ningún post.
+SELECT *
+FROM usuario
+LEFT JOIN post 
+ON usuario.id = post.usuario_id
+WHERE post.usuario_id IS NULL;
+-- 7. Listar todos los post con sus comentarios (incluyendo aquellos que no poseen
+-- comentarios).
+SELECT *
+FROM post
+FULL OUTER JOIN comentario
+ON post.id = comentario.post_id;
+
+-- SELECT *
+-- FROM post
+-- LEFT JOIN comentario
+-- ON post.id = comentario.post_id;
+
+-- SELECT *
+-- FROM comentario
+-- LEFT JOIN post
+-- ON post.id = comentario.post_id;
+-- 8. Listar todos los usuarios que hayan publicado un post en Junio.
+-- SELECT usuario.*, post.fecha
+-- FROM usuario
+-- JOIN post 
+-- ON usuario.id = post.usuario_id
+-- WHERE EXTRACT(MONTH FROM post.fecha) = 6;
+
+SELECT usuario.*
+FROM usuario
+JOIN post
+ON usuario.id = post.usuario_id
+WHERE post.fecha BETWEEN '2020-06-01' AND '2020-06-30';
+
